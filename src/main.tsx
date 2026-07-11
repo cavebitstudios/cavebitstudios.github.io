51180d3a690e8a9a4c8feceaffd8c811fbb898d7
caffinecavern <cavebitstudios@gmail.com>
2026-07-11 16:11:32 -0400

    Hotfixes

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,16 @@ import { HashRouter } from 'react-router-dom'
 import { App } from './App'
 import './styles/globals.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>,
+const root = document.getElementById('root')
+
+if (!root) {
+    throw new Error('Root element was not found')
+}
+
+createRoot(root).render(
+    <StrictMode>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </StrictMode>,
 )
